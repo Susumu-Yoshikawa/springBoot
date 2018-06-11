e179ea2f5210ccf5ff0a58a324d59ecdf502b5ec
yoshikawasusumu <yoshikawasusumu@192.168.23.150>
2018-06-11 16:29:15 +0900

    4.1_2

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -10,17 +10,50 @@ public class HelloController {
 	@RequestMapping(value="/")
 	public ModelAndView index(ModelAndView mav){
 		mav.setViewName("index");
+		mav.addObject("msg","current data.");
+		DataObject obj = new DataObject(123, "hanko", "hanko@mail.com");
+		mav.addObject("object",obj);
 		return mav;
 	}
 
-	@RequestMapping(value="/other")
-	public String other(){
-		return "redirect:/";
+}
+
+class DataObject{
+
+	private int id;
+	private String name;
+	private String value;
+
+	public DataObject(int id, String name, String value) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.value = value;
 	}
 
-	@RequestMapping(value="/home")
-	public String home(){
-		return "forward:/";
+	public int getId() {
+		return id;
 	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
 
 }
