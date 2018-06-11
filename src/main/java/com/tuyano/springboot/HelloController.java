95fed3de71f5c95eb189878eb1c794d5fe46a58c
yoshikawasusumu <yoshikawasusumu@192.168.23.150>
2018-06-11 13:45:13 +0900

    3.4_model

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,58 +1,22 @@
 package com.tuyano.springboot;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
-	String[] names = {"tuyano","hanako","taro","sachiko","ichiro"};
-	String[] mails = {"syoda@mail.com","hanako@mail.com","taro@yamada","sachiko@111","イチロー@uho"};
+	@RequestMapping("/{num}")
+	public String index(@PathVariable int num, Model model){
+		int res = 0;
+		for(int i = 1; i <= num; i++) {
+			res += i;
+		}
 
-	@RequestMapping("/{id}")
-	public DataObject index(@PathVariable int id){
-		return new DataObject(id,names[id],mails[id]);
+		model.addAttribute("msg", "total: "+res);
+
+		return "index";
 	}
 }
-
-class DataObject{
-
-	private int id;
-	private String name;
-	private String value;
-
-	public DataObject(int id, String name, String value) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.value = value;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
-
-}
-
