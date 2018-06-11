92984cd4074466fe2da4e30ce6981a94ff33d941
yoshikawasusumu <yoshikawasusumu@192.168.23.150>
2018-06-11 17:02:18 +0900

    4.2_2

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,20 +1,23 @@
 package com.tuyano.springboot;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
 
-	@RequestMapping(value="/{id}")
-	public ModelAndView index(@PathVariable int id, ModelAndView mav){
+	@RequestMapping(value="/")
+	public ModelAndView index(ModelAndView mav){
 		mav.setViewName("index");
-		mav.addObject("id",id);
-		mav.addObject("check",id % 2 ==0);
-		mav.addObject("trueVal","Even number!");
-		mav.addObject("falseVal","Odd number...");
+		ArrayList<String[]> data = new ArrayList<String[]>();
+		data.add(new String[] {"taro","taro@mail.com","090-9999-9999"});
+		data.add(new String[] {"hanako","hanako@mail.com","190-9999-9999"});
+		data.add(new String[] {"sachiko","sachi@mail.com","290-9999-9999"});
+		System.out.println(data);
+		mav.addObject("data",data);
 		return mav;
 	}
 
