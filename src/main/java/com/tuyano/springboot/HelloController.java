ace39274df70dc0b8f57f5f3feadfe198ca994e7
yoshikawasusumu <yoshikawasusumu@192.168.23.150>
2018-06-12 11:41:40 +0900

    5.1

--- src/main/java/com/tuyano/springboot/HelloController.java
+++ src/main/java/com/tuyano/springboot/HelloController.java
@@ -1,15 +1,24 @@
 package com.tuyano.springboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tuyano.springboot.repositories.MyDataRepository;
+
 @Controller
 public class HelloController {
+
+	@Autowired
+	MyDataRepository repository;
 
 	@RequestMapping(value="/")
 	public ModelAndView index(ModelAndView mav){
 		mav.setViewName("index");
+		mav.addObject("msg","this is sample content.");
+		Iterable<MyData> list = repository.findAll();
+		mav.addObject("data",list);
 		return mav;
 	}
 
